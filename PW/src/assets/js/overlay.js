dc36d906657b51523756e0cd3f205c2562cb2dca
Jonathan-Schnee <jonathan.schnee@hs-furtwangen.de>
2023-11-14 21:17:33 +0100

    menu works

--- PW/src/assets/js/overlay.js
+++ PW/src/assets/js/overlay.js
@@ -1,28 +1,41 @@
-var subnav = document.getElementById("dropdown-menu-1");
-subnav.style.visibility = "hidden";
-async function openDropdown(arg){
-    const navbar = document.getElementsByClassName('navbar-nav');
-    const colnavbar = document.getElementById('navbarNavDropdown')
-    subnav = document.getElementById(arg);
-    subnav.position = 'absolute';
-    var l = navbar[0].getBoundingClientRect().left - subnav.parentElement.getBoundingClientRect().left;
-    var r = navbar[0].style.width;
-    subnav.style.left = l +"px";
-    subnav.style.width = navbar[0].offsetWidth +"px";
-    
-    if(window.screen.width >= 991.98 ){
-        subnav.style.visibility = subnav.style.visibility == "hidden" ? "visible" : "hidden";
 
-        if(subnav.style.visibility == "visible"){
-            subnav.style.maxHeight = "800px";
-        }
-        else{
-            subnav.style.maxHeight = "0px";
-        }
-        await new Promise(r => setTimeout(r, 500));
-        colnavbar.style.paddingBottom = subnav.offsetHeight + 'px';
-        colnavbar.style.transition = "all 0.5s"    
+var  navbar;
+var  colnavbar;
+var subnavs = ['dropdown-menu-1', 'dropdown-menu-2'];
+var subnav
+var dropdown;
+var bool = false;
+function init(){
+    if(window.innerWidth >= 991.98){
+        navbar = document.getElementsByClassName('navbar-nav');
+        colnavbar = document.getElementById('navbarNavDropdown');
+        subnavs.forEach(function (subnav){
+            subnav = document.getElementById(subnav);
+            dropdown = subnav.parentElement;
+            var l = navbar[0].getBoundingClientRect().left - dropdown.getBoundingClientRect().left;
+            var r = navbar[0].style.width;
+            dropdown.style.maxHeight = "0px";
+            dropdown.style.left = l +"px";
+            dropdown.style.width = navbar[0].offsetWidth +"px";
+            dropdown.style.transition = "all 3s";
+        });        
+        colnavbar.style.transition = "all 3s"
     }
-
+}
+function openDropdown(arg){
+    if(window.innerWidth >= 991.98){
+        subnav = document.getElementById(arg)
+        dropdown = subnav.parentElement
+        dropdown.style.maxHeight = subnav.offsetHeight + 'px';
+        colnavbar.style.paddingBottom = subnav.offsetHeight + 'px';
+    }
+}
+function closeDropdown(arg){
+    if(window.innerWidth >= 991.98){
+        subnav = document.getElementById(arg)
+        dropdown = subnav.parentElement
+        dropdown.style.maxHeight = '0px';
+        colnavbar.style.paddingBottom = '0px';
+    }
 }
 
