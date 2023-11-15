d002db4ea8bf880a1f267ddf0c6cc0ee60f09f51
Jonathan-Schnee <jonathan.schnee@hs-furtwangen.de>
2023-11-15 13:17:27 +0100

    menu now completely working (exept responsive)

--- PW/src/assets/js/overlay.js
+++ PW/src/assets/js/overlay.js
@@ -1,9 +1,11 @@
 
 var  navbar;
 var  colnavbar;
+var navLinks = ['nav-link-1','nav-link-2']
 var subnavs = ['dropdown-menu-1', 'dropdown-menu-2'];
 var subnav
 var dropdown;
+var menuTransition = 'all 0.5s'
 var bool = false;
 function init(){
     if(window.innerWidth >= 991.98){
@@ -14,18 +16,34 @@ function init(){
             dropdown = subnav.parentElement;
             var l = navbar[0].getBoundingClientRect().left - dropdown.getBoundingClientRect().left;
             var r = navbar[0].style.width;
+            dropdown.style.transition = 'none';
             dropdown.style.maxHeight = "0px";
-            dropdown.style.left = l +"px";
+            dropdown.style.left += l +"px";
             dropdown.style.width = navbar[0].offsetWidth +"px";
-            dropdown.style.transition = "all 3s";
+            dropdown.offsetHeight;
+            dropdown.style.transition = menuTransition;
         });        
-        colnavbar.style.transition = "all 3s"
+        colnavbar.style.transition = menuTransition;
+        navLinks.forEach(function(navLink){
+            document.getElementById(navLink).style.pointerEvents = 'auto';
+        })       
+    }
+    else{
+        subnavs.forEach(function (subnav){
+            subnav = document.getElementById(subnav);
+            dropdown = subnav.parentElement;
+            dropdown.style.transition = 'none';
+            dropdown.style.maxHeight = "";
+            dropdown.style.width = "auto";
+        });   
+
     }
 }
 function openDropdown(arg){
     if(window.innerWidth >= 991.98){
         subnav = document.getElementById(arg)
         dropdown = subnav.parentElement
+        
         dropdown.style.maxHeight = subnav.offsetHeight + 'px';
         colnavbar.style.paddingBottom = subnav.offsetHeight + 'px';
     }
