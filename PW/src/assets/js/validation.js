5ce0515c954cc5ae1a031fa09df203694bd7d509
Jonathan-Schnee <jonathan.schnee@hs-furtwangen.de>
2023-12-08 12:01:30 +0100

    added email fix

--- PW/src/assets/js/validation.js
+++ PW/src/assets/js/validation.js
@@ -4,19 +4,7 @@ function check() {
   
     // Fetch all the forms we want to apply custom Bootstrap validation styles to
     var forms = document.querySelectorAll('.needs-validation')
-    const inputs = Array.from(
-      document.querySelectorAll('input[name=telephone], input[name=email]')
-    );
-    console.log(telephone.checkValidity())
-    console.log(email.checkValidity())
-    
-    const inputListener = e => {
-      inputs
-        .filter(i => i !== e.target)
-        .forEach(i => (i.required = !e.target.value.length));
-    };
-  
-    inputs.forEach(i => i.addEventListener('input', inputListener));
+
 
     // Loop over them and prevent submission
     Array.prototype.slice.call(forms)
